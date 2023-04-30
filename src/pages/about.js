511a289f63d033a4b11bd08f93242bd994204b7e
aaronjtonner <tonneraaron@gmail.com>
2023-04-29 20:28:04 -0600

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import { Container, Section } from 'components/layoutComponents'
+import React from 'react'
+
+export default function About() {
+  return (
+    <Section>
+        <Container>
+            <p>hi</p>
+        </Container>
+    </Section>
+  )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+
+import HeroSlider from 'components/heroSlider'
+import React from 'react'
+
+export default function index() {
+  return (
+    <div>
+       <p>home</p>
+    </div>
+  )
+}
